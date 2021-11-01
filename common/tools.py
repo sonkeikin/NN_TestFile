93699fa898384fbb5eaea05803fde3e3198ade1f
keikin.son <76418465+sonkeikin@users.noreply.github.com>
2021-11-01 23:58:01 +0900

    Add files via upload

---
+++ common/tools.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def point_append(add,train_point):
+        if add not in train_point:
+            train_point.append(add)
+        elif add == 1:
+            over_add = [num for num in train_point if 1<num<2]
+            for i in over_add:
+                train_point.remove(i)
+            add_dx = 1 / (len(over_add) + 2) 
+            add += add_dx
+            while 1< add < 2-0.001:
+                train_point.append(add)
+                add += add_dx  
+        elif add == 20:
+            over_add = [num for num in train_point if 19<num<20]
+            for i in over_add:
+                train_point.remove(i)
+            add_dx = 1 / (len(over_add) + 2) 
+            add += add_dx - 1
+            while 19< add < 20-0.001:
+                train_point.append(add)
+                add += add_dx
+        else:
+            over_add = [num for num in train_point if add - 1 <num< add + 1]
+            for i in over_add:
+                train_point.remove(i)
+            train_point.append(add)
+            print(len(over_add))
+            if len(over_add) == 1:
+                train_point.append(add - 0.5)
+                train_point.append(add + 0.5)
+            else:
+                add_dx = 1 / ((len(over_add) - 1) / 2 + 2)
+
+                add_minus = (add - 1 ) + add_dx
+                while add - 1 < add_minus < add -0.001:
+                    train_point.append(add_minus)
+                    add_minus += add_dx
+
+                add_plus = add + add_dx
+                while add < add_plus < add + 1 -0.001:
+                    train_point.append(add_plus)
+                    add_plus += add_dx
+        return train_point
+
+def get_max_point(test_max_loss_list):
+    tmp = {i:test_max_loss_list.count(i) for i in set(test_max_loss_list)}
+    return max(zip(tmp.values(), tmp.keys()))[1]
+
+def get_pic(content,time,label = "blank" , x_label = "x" , y_label = "y"):
+    fig1 = plt.figure(1)
+    x = np.arange(len(content))
+    plt.plot(x, content, label='test max loss', linestyle='--')
+    plt.xlabel(x_label)
+    plt.ylabel(x_label)
+    plt.ylim(0, max(content))
+    plt.legend(loc='upper right')
+    label += str(time)
+    plt.savefig('%s'%label , dpi=300) #指定分辨率保存 
+    plt.draw()
+    plt.pause(1)
+    plt.close(fig1)
